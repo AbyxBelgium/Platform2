a652b06248bb3c6d434d5884774808f1a9aae92f
pverscha <Pieter.Verschaffelt@UGent.be>
2018-06-15 19:52:04 +0200

    Link to live website in backend

--- resources/views/backend/includes/nav.blade.php
+++ resources/views/backend/includes/nav.blade.php
@@ -1,4 +1,4 @@
-<a class="mdl-navigation__link" href="{{ route('backend/index')  }}"><span class="icon icon-home"></span>Desktop</a>
+<a class="mdl-navigation__link" href="{{ route('backend/index')  }}"><span class="icon icon-home"></span> Desktop</a>
 <a class="mdl-navigation__link" href="#" id="submenu-new">
     <span class="icon icon-add"></span> New
 </a>
@@ -13,4 +13,5 @@
     <li class="mdl-menu__item mdl-js-ripple-effect" tabindex="-1"><a class="no-link" href="{{ route('backend/post/index', ['page' => 1]) }}">Posts</a></li>
     <li class="mdl-menu__item mdl-js-ripple-effect" tabindex="-1"><a class="no-link" href="{{ route('backend/category/index', ['page' => 1]) }}">Categories</a></li>
 </ul>
-<a class="mdl-navigation__link" href="/logout"><span class="icon icon-play_for_work"></span>Logout</a>
+<a class="mdl-navigation__link" href="{{ route('frontend/index') }}"><span class="icon icon-screen_share"></span> Live website</a>
+<a class="mdl-navigation__link" href="/logout"><span class="icon icon-play_for_work"></span> Logout</a>
