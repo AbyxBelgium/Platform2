b8c691eac392836244138ec5679f8252acc09fed
pverscha <Pieter.Verschaffelt@UGent.be>
2018-06-19 19:29:57 +0200

    Part of upload image form implemented

--- resources/views/backend/pages/image/index.blade.php
+++ resources/views/backend/pages/image/index.blade.php
@@ -12,13 +12,14 @@
             <span class="icon icon-add icon-fix-position"></span> Upload new
         </a>
 
-        <form method="post" enctype="multipart/form-data" novalidate="" class="box">
+        <!-- Advanced file upload form. Based on https://css-tricks.com/drag-and-drop-file-uploading/ -->
+        <form method="post" enctype="multipart/form-data" action="{{ route('backend/image/create') }}" novalidate="" class="box box__invisible">
             <div class="box__input">
                 <svg class="box__icon" xmlns="http://www.w3.org/2000/svg" width="50" height="43" viewBox="0 0 50 43">
                     <path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z"></path>
                 </svg>
                 <input type="file" name="files[]" id="file" class="box__file" data-multiple-caption="{count} files selected" multiple="">
-                <label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
+                <label for="file"><strong>Choose an image</strong><span class="box__dragndrop"> or drag it here</span>.</label>
                 <button type="submit" class="box__button">Upload</button>
             </div>
             <div class="box__uploading">Uploading…</div>
@@ -70,6 +71,10 @@
             transition: outline-offset .15s ease-in-out, background-color .15s linear;
         }
 
+        .box__invisible {
+            display: none;
+        }
+
         .box.is-dragover {
             outline-offset: -20px;
             outline-color: #c8dadf;
@@ -88,6 +93,14 @@
         .box__success,
         .box__error {
             display: none;
+        }
+
+        .box.is-uploading .box__input {
+            visibility: hidden;
+        }
+
+        .box.is-uploading .box__uploading {
+            display: block;
         }
 
         .box__file {
@@ -129,4 +142,73 @@
             margin: 40px auto 0;
         }
     </style>
+@stop()
+
+@section('javascript')
+    <script>
+        let $addButton = $('.create-button');
+        $addButton.click(function() {
+            $('.box').toggleClass('box__invisible')
+        });
+
+        // Start of upload controlling code
+        let $form = $('.box');
+
+        let droppedFiles = false;
+
+        $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+        })
+            .on('dragover dragenter', function() {
+                $form.addClass('is-dragover');
+            })
+            .on('dragleave dragend drop', function() {
+                $form.removeClass('is-dragover');
+            })
+            .on('drop', function(e) {
+                droppedFiles = e.originalEvent.dataTransfer.files;
+                $form.trigger('submit');
+            });
+
+        $form.on('submit', function(e) {
+            if ($form.hasClass('is-uploading')){
+                return false;
+            }
+
+            $form.addClass('is-uploading').removeClass('is-error');
+
+            e.preventDefault();
+
+            let ajaxData = new FormData($form.get(0));
+
+            let $input = $('input[type="file"]');
+
+            if (droppedFiles) {
+                $.each( droppedFiles, function(i, file) {
+                    ajaxData.append($input.attr('name'), file);
+                });
+            }
+
+            console.log('Going to perform an AJAX!');
+
+            $.ajax({
+                url: $form.attr('action'),
+                type: $form.attr('method'),
+                data: ajaxData,
+                dataType: 'json',
+                cache: false,
+                contentType: false,
+                processData: false,
+                complete: function() {
+                    $form.removeClass('is-uploading');
+                },
+                success: function(data) {
+                    $form.addClass(data.success === true ? 'is-success' : 'is-error' );
+                    if (!data.success) $errorMsg.text(data.error);
+                }
+            });
+        });
+        // End of upload controlling code
+    </script>
 @stop()
