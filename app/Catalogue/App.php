8bf6530e9979d790f5cdca8877518393d97c78d4
Pieter Verschaffelt <pverschaffelt@securecodewarrior.com>
2018-07-08 19:54:43 +0200

    Apps now have a install and uninstall callback

--- app/Catalogue/App.php
+++ app/Catalogue/App.php
@@ -1,5 +1,8 @@
 <?php
 /**
+ * Every App should expand this class, as it's used by the system for optimal
+ * installation and configuration.
+ *
  * @author Pieter Verschaffelt
  */
 
@@ -12,6 +15,20 @@ abstract class App
 {
     public abstract function getName(): string;
     public abstract function getElements(): array;
+
+    /**
+     * This method is called when the App is installed into the system and
+     * should be used for registering and installing persistent features, such
+     * as database tables.
+     */
+    public function onInstall(): void {}
+
+    /**
+     * This method is called when the App is uninstalled from the system and
+     * should be used for deregistering and removing persistent features such
+     * as database tables.
+     */
+    public function onUninstall(): void {}
 
     public function getElementByIdentifier(string $identifier): Element
     {
