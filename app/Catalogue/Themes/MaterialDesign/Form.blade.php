20beffd3c9556aeeaafce96877d168f8396a10a3
Pieter Verschaffelt <pieterverschaffelttwee@hotmail.com>
2018-08-18 10:19:15 +0200

    More work on extensions

--- app/Catalogue/Themes/MaterialDesign/Form.blade.php
+++ app/Catalogue/Themes/MaterialDesign/Form.blade.php
@@ -2,7 +2,7 @@
     @foreach($element->getInputs() as $inp)
         {!! $inp->render() !!}
     @endforeach
-    <div class="center" style="padding-top: 10px;">
+    <div class="center">
         <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored" id="publish-button" type="submit">
             {{ $element->getSubmitText() }}
         </button>
