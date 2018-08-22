edaebcb695acc5f4331d07201a5b8d8525f4910d
Pieter Verschaffelt <pieterverschaffelttwee@hotmail.com>
2018-08-22 18:50:06 +0200

    Removed DebugBar from app.php

--- config/app.php
+++ config/app.php
@@ -160,7 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\SystemResourceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+        //Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -209,7 +209,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class
+        //'Debugbar' => Barryvdh\Debugbar\Facade::class
     ],
 
 ];
